a31a969052535bf444ef28bad034723d216b71bd
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-10 17:20:01 +0000

    scraper

--- lib/local_area_meetups.rb
+++ lib/local_area_meetups.rb
@@ -5,3 +5,13 @@ require 'pry'
 require_relative  "./local_area_meetups/version"
 require_relative  './local_area_meetups/meetups'
 require_relative  './local_area_meetups/cli'
+
+class LocalAreaMeetups
+
+  def get_page(https://www.meetup.com/)
+    doc = Nokogiri::HTML(open(https://www.meetup.com/))
+ binding.pry
+    scraped_categories = []
+
+  end
+end
