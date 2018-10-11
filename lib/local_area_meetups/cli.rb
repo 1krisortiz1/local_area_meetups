84a0bcb67b656e12b28a3df7606b0d24e6831b2a
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-11 22:53:54 +0000

    Created menu method to navigate user options.  Created loop to request input for more info or exit to exit.

--- lib/local_area_meetups/cli.rb
+++ lib/local_area_meetups/cli.rb
@@ -1,12 +1,31 @@
 class LocalAreaMeetups::CLI
 
   def call
-    puts "Local Meetups"
+
     list_categories
+    menu
   end
 
   def list_categories
-
+    puts "Local Meetups"
+    puts <<~DOC
+      1. Outdoors & Adventure
+      2. Tech
+      3. Family
+    DOC
   end
 
+  def menu
+      puts "Choose from the category list (1 - 24) or type exit to exit:"
+        input = nil
+        while input != "exit"
+        input = gets.strip.downcase
+        case input
+        when "1"
+          puts "Here are meetups in the Outdoors & Adventure category"
+        when "2"
+          puts "Here are meetups in the Tech category"
+        end
+      end
+  end
 end
