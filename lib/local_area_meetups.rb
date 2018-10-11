8d072dc4d5e05cbb1f011a3a254954076b621770
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-11 22:28:24 +0000

    Encapsulated cli in bin file.  Defined call method, beginning to build app with fake data.

--- lib/local_area_meetups.rb
+++ lib/local_area_meetups.rb
@@ -1,17 +1,8 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
+require_relative "./local_area_meetups/version"
+require_relative "./local_area_meetups/cli"
 
-require_relative  "./local_area_meetups/version"
-require_relative  './local_area_meetups/meetups'
-require_relative  './local_area_meetups/cli'
 
-class LocalAreaMeetups
 
-  def get_page(https://www.meetup.com/)
-    doc = Nokogiri::HTML(open(https://www.meetup.com/))
- binding.pry
-    scraped_categories = []
+module LocalAreaMeetups
 
-  end
 end
--- lib/local_area_meetups/cli.rb
+++ lib/local_area_meetups/cli.rb
@@ -1,38 +1,12 @@
-#CLI controller
 class LocalAreaMeetups::CLI
 
   def call
+    puts "Local Meetups"
     list_categories
-    menu
-    goodbye
   end
 
   def list_categories
-    puts "Local Area Meetups"
-    @meetups = LocalAreaMeetups::Meetup.current_meetups
-    @meetups.each.with_index(1) do |meetup, i|
-      puts "#{i}. #{meetup.category} - #{meetup.url}"
-    end
+
   end
 
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Choose from the categories listed (1-24) or type list to see the categories again or type exit:"
-      input = gets.strip.downcase
-
-      if input.to_i > 0
-        the_meetup = @meetups[input.to_i-1]
-        puts "#{meetup.category} - #{meetup.url}"
-      elsif input == "list"
-        list_categories
-      else
-        puts "Not sure what you want type list or exit"
-      end
-    end
-  end
-
-  def goodbye
-    puts "See you next time!"
-  end
 end
