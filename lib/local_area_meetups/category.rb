e749e56df618f23bc0ba47b066adae8a5abb8502
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-13 17:38:22 +0000

    added exit into while loop. removed goodby from call method to avoid duplicate output of exit message.

--- lib/local_area_meetups/category.rb
+++ lib/local_area_meetups/category.rb
@@ -1,27 +1,12 @@
 class LocalAreaMeetups::Category
-  #attr_accessor :name
+  attr_accessor :name
 
   def self.categories
     doc = Nokogiri::HTML(open("https://www.meetup.com"))
-    doc.search("h4.text--bold").children
-    binding.pry
-
-    #scraped_categories = []
+    category_name = doc.search("h4.text--bold").children
+    #binding.pry
 
 
-    #category_1 = self.new
-    #category_1.name = "Outdoors & Adventure"
-
-    ##category_2 = self.new
-    ##category_2.name = "Tech"
-
-    ##category_3 = self.new
-    ##category_3.name = "Family"
-
-    ##category_4 = self.new
-    ##category_4.name = "Health & Wellness"
-
-    #[category_1, category_2, category_3, category_4]#
 
   end
 end
--- lib/local_area_meetups/cli.rb
+++ lib/local_area_meetups/cli.rb
@@ -3,47 +3,35 @@ class LocalAreaMeetups::CLI
   def call
     list_categories
     menu
-    list_meetups
-    goodbye
+    #list_meetups
+    #goodbye
+  end
+
+  def list_categories
+    puts "Local Meetup Categories:"
+    @category = LocalAreaMeetups::Category.categories
+    @category.each.with_index(1) do |category, i|
+      puts "#{i}. #{category.name}"
+    end
   end
 
   def menu
-        #input = nil
-      #while input != "exit"
-          puts "Choose from the category list (1 - 24) or list to view the categories again or type exit to exit:"
-          #input = gets.strip.downcase
-          #if input.to_i > 0
-            #the_category = @category[input.to_i-1]
-            #puts "#{the_category.name}"
-            #the_meetup = @meetup[input.to_i-1]
-            #puts "#{the_meetup.name} - #{the_meetup.group_name} - #{the_meetup.meetup_location}, #{the_meetup.meetup_date_and_time}, #{the_meetup.url}"
-
-            #meetup_input = gets.strip.downcase
-
-          #elsif input == "list"
-            #list_categories
-          #else
-            #puts "Please enter a valid response!"
-          #end
-      #end
-  end
-
-
-    def list_categories
-      puts "Local Meetup Categories:"
-      @category = LocalAreaMeetups::Category.categories
-      @category.each.with_index(1) do |category, i|
-        puts "#{i}. #{category.name}"
+    input = nil
+    while input != "exit"
+      puts "Choose from the category list (1 - 24) or list to view the categories again or type exit to exit:"
+      input = gets.strip.downcase
+      if input.to_i > 0 && input.to_i < 25
+        the_category = @category[input.to_i-1]
+        puts "#{the_category}"
+      elsif input == "list"
+        list_categories
+      elsif input == "exit"
+        goodbye
+      else
+        puts "Please enter a valid response!"
       end
     end
-
-    def list_meetups
-        puts "Here are meetups in the area:"
-        @meetup = LocalAreaMeetups::Meetup.category_meetups
-        @meetup.each.with_index(1) do |meetup, i|
-          puts "#{i}. #{meetup.name} - #{meetup.group_name} - #{meetup.meetup_location}, #{meetup.meetup_date_and_time}, #{meetup.url}"
-        end
-    end
+  end
 
   def goodbye
     puts "Come back again for more meetups!!!"
