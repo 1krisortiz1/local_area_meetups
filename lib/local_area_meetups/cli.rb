4b37130c9667794971507b2a1670578afbaa232c
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-14 04:45:36 +0000

    updating meetup class

--- lib/local_area_meetups/cli.rb
+++ lib/local_area_meetups/cli.rb
@@ -3,8 +3,6 @@ class LocalAreaMeetups::CLI
   def call
     list_categories
     menu
-
-
   end
 
   def list_categories
@@ -17,11 +15,13 @@ class LocalAreaMeetups::CLI
   end
 
   def list_meetups
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
     @meetup = LocalAreaMeetups::Meetup.category_meetups
     @meetup.each.with_index(1) do |meetup, i|
       puts "#{i}. #{meetup.group_name} - #{meetup.group_location} - #{meetup.member_count} - #{meetup.group_status}"
     end
-    puts "Enjoy!!"
+      puts "Enjoy!!"
   end
 
   def menu
@@ -31,15 +31,53 @@ class LocalAreaMeetups::CLI
       input = gets.strip.downcase
 
       if input == "1"
-        puts "Here is a list of meetups:"
-        puts "Loading..................."
-        list_meetups
-        #the_category = @category[input.to_i-1]
-        #the_meetup = @meetup[input.to_i-1]
-        #puts "Click the link for meetups in your area!"
-        #puts "#{the_category.name} - #{the_category.url}"
+        list_meetups #Outdoors & Adventure Meetups
       elsif input == "2"
-        list_meetups
+        list_meetups #Tech Meetups
+      elsif input == "3"
+        list_meetups #Family Meetups
+      elsif input == "4"
+        list_meetups #Health & Wellness Meetups
+      elsif input == "5"
+        list_meetups #Learning Meetups
+      elsif input == "6"
+        list_meetups #Sports & Fitness Meetups
+      elsif input == "7"
+        list_meetups #Photography Meetups
+      elsif input == "8"
+        list_meetups #Food & Drink Meetups
+      elsif input == "9"
+        list_meetups #Writing Meetups
+      elsif input == "10"
+        list_meetups #Language & Culture Meetups
+      elsif input == "11"
+        list_meetups #Music Meetups
+      elsif input == "12"
+        list_meetups #Movements Meetups
+      elsif input == "13"
+        list_meetups #LGBTQ Meetups
+      elsif input == "14"
+        list_meetups #Film Meetups
+      elsif input == "15"
+        list_meetups #Sci-Fi & Games Meetups
+      elsif input == "16"
+        list_meetups #Beliefs Meetups
+      elsif input == "17"
+        list_meetups #Arts Meetups
+      elsif input == "18"
+        list_meetups #Book Clubs Meetups
+      elsif input == "19"
+        list_meetups #Dance Meetups
+      elsif input == "20"
+        list_meetups #Pets Meetups
+      elsif input == "21"
+        list_meetups #Hobbies & Crafts Meetups
+      elsif input == "22"
+        list_meetups #Fashion & Beauty Meetups
+      elsif input == "23"
+        list_meetups #Social Meetups
+      elsif input == "24"
+        list_meetups #Career & Business Meetups
       elsif input == "list"
         list_categories
       elsif input == "exit"
@@ -49,8 +87,7 @@ class LocalAreaMeetups::CLI
       end
     end
   end
-        #puts "Select a meetup group for more information"
-        #puts "#{meetup.group_name} - #{meetup.group_location} - #{meetup.member_count} - #{meetup.group_status}"
+
   def goodbye
     puts "Come back again for more meetups!!!"
   end
--- lib/local_area_meetups/meetup.rb
+++ lib/local_area_meetups/meetup.rb
@@ -1,126 +1,117 @@
 class LocalAreaMeetups::Meetup
   attr_accessor :group_name, :group_location, :url, :member_count, :group_status
 
+  @meetup_meetups = []
+
   def self.category_meetups
     self.scrape_meetups
   end
 
-  def self.scrape_meetups
-    meetup = []
-
-    meetup << self.scrape_hiking_hikers_hiking_group
-    meetup << self.scrape_global_bikes_meetup
-    meetup << self.scrape_trail_mix_hiking
-    meetup << self.scrape_twenties_and_thirties_hikers_meetup
-    meetup << self.scrape_performance_great_ride
-    meetup << self.scrape_phoenix_beginner_hiking
-    meetup << self.scrape_take_a_hike
-    meetup << self.scrape_a_little_off_the_beaten_path
-
-    meetup
+  def self.scrape_outdoor_meetups
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/outdoors-adventure/"))
+    i = 0
+    while i < 6
+      outdoor_meetup = self.new
+      outdoor_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      outdoor_meetup.url = "https://www.meetup.com/find/outdoors-adventure"
+      @meetup_meetups << outdoor_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_hiking_hikers_hiking_group
-    doc = Nokogiri::HTML(open("https://www.meetup.com/threeh/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_tech_meetups
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/tech"))
+    i = 0
+    while i < 6
+      tech_meetup = self.new
+      tech_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      tech_meetup.url = "https://www.meetup.com/find/tech"
+      @meetup_meetups << tech_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_global_bikes_meetup
-    doc = Nokogiri::HTML(open("https://www.meetup.com/Global-Bikes-Meetup/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_family_meetups
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/parents-family"))
+    i = 0
+    while i < 6
+      family_meetup = self.new
+      family_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      family_meetup.url = "https://www.meetup.com/find/parents-family"
+      @meetup_meetups << family_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_trail_mix_hiking
-    doc = Nokogiri::HTML(open("https://www.meetup.com/Trail-Mix-Hiking/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_health_and_wellness
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/health-wellness"))
+    i = 0
+    while i < 6
+      health_meetup = self.new
+      health_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      health_meetup.url = "https://www.meetup.com/find/health-wellness"
+      @meetup_meetups << health_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_twenties_and_thirties_hikers_meetup
-    doc = Nokogiri::HTML(open("https://www.meetup.com/20s-and-30s-Hikers-Meetup-Phoenix-Az/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_learning_meetups
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/education"))
+    i = 0
+    while i < 6
+      learning_meetup = self.new
+      learning_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      learning_meetup.url = "https://www.meetup.com/find/education"
+      @meetup_meetups << learning_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_performance_great_ride
-    doc = Nokogiri::HTML(open("https://www.meetup.com/Performance-Saturday-Social-Group-Ride/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_sports_and_fitness
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/sports-fitness"))
+    i = 0
+    while i < 6
+      sports_meetup = self.new
+      sports_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      sports_meetup.url = "https://www.meetup.com/find/sports-fitness"
+      @meetup_meetups << sports_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_phoenix_beginner_hiking
-    doc = Nokogiri::HTML(open("https://www.meetup.com/phoenix-beginner-hikers/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_photograpy
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/photography"))
+    i = 0
+    while i < 6
+      photography_meetup = self.new
+      photography_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      photography_meetup.url = "https://www.meetup.com/find/photography"
+      @meetup_meetups << photography_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_take_a_hike
-    doc = Nokogiri::HTML(open("https://www.meetup.com/Take-a-Hike/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_food
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/food"))
+    i = 0
+    while i < 6
+      food_meetup = self.new
+      food_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      food_meetup.url = "https://www.meetup.com/find/food"
+      @meetup_meetups << food_meetup
+      i += 1
+    end
   end
 
-  def self.scrape_a_little_off_the_beaten_path
-    doc = Nokogiri::HTML(open("https://www.meetup.com/A-Little-Off-the-Beaten-Path/events"))
-
-    meetup = self.new
-    meetup.group_name = doc.search("a.groupHomeHeader-groupNameLink").text
-    meetup.group_location = doc.search("a.groupHomeHeaderInfo-cityLink").text
-    meetup.member_count = doc.search("a.groupHomeHeaderInfo-memberLink").text
-    meetup.url = "https://www.meetup.com/find/outdoors-adventure"
-    meetup.group_status = doc.search("span.infoToggle-label").text
-
-    meetup
+  def self.scrape_writing
+    doc = Nokogiri::HTML(open("https://www.meetup.com/find/writing"))
+    i = 0
+    while i < 6
+      writing_meetup = self.new
+      writing_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      writing_meetup.url = "https://www.meetup.com/find/writing"
+      @meetup_meetups << writing_meetup
+      i += 1
+    end
   end
 end
