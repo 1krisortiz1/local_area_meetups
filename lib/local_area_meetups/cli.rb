c3d94b177309b3db7d7850159eada9bc8fcf0896
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-22 02:00:19 +0000

    Completed meetup class. test cli program additional work needed on the print out of the meetup selection.

--- lib/local_area_meetups/cli.rb
+++ lib/local_area_meetups/cli.rb
@@ -14,16 +14,6 @@ class LocalAreaMeetups::CLI
     end
   end
 
-  def list_meetups
-    puts "Here is a list of meetups:"
-    puts "Loading..................."
-    @meetup = LocalAreaMeetups::Meetup.meetups
-    @meetup.each.with_index(1) do |meetup, i|
-    puts "#{i}. #{meetup.group_name} - #{meetup}"
-    end
-      puts "Enjoy!!"
-  end
-
   def menu
     input = nil
     while input != "exit"
@@ -31,53 +21,53 @@ class LocalAreaMeetups::CLI
       input = gets.strip.downcase
 
       if input == "1"
-           list_meetups#Outdoors & Adventure Meetups
+          list_meetups_outdoor#Outdoors & Adventure Meetups
       elsif input == "2"
-        list_meetups #Tech Meetups
+        list_meetups_tech #Tech Meetups
       elsif input == "3"
-        list_meetups #Family Meetups
+        list_meetups_family #Family Meetups
       elsif input == "4"
-        list_meetups #Health & Wellness Meetups
+        list_meetups_health #Health & Wellness Meetups
       elsif input == "5"
-        list_meetups #Learning Meetups
+        list_meetups_learning #Learning Meetups
       elsif input == "6"
-        list_meetups #Sports & Fitness Meetups
+        list_meetups_sports #Sports & Fitness Meetups
       elsif input == "7"
-        list_meetups #Photography Meetups
+        list_meetups_photography #Photography Meetups
       elsif input == "8"
-        list_meetups #Food & Drink Meetups
+        list_meetups_food #Food & Drink Meetups
       elsif input == "9"
-        list_meetups #Writing Meetups
+        list_meetups_writing #Writing Meetups
       elsif input == "10"
-        list_meetups #Language & Culture Meetups
+        list_meetups_language_culture #Language & Culture Meetups
       elsif input == "11"
-        list_meetups #Music Meetups
+        list_meetups_music #Music Meetups
       elsif input == "12"
-        list_meetups #Movements Meetups
+        list_meetups_movements #Movements Meetups
       elsif input == "13"
-        list_meetups #LGBTQ Meetups
+        list_meetups_lgbtq #LGBTQ Meetups
       elsif input == "14"
-        list_meetups #Film Meetups
+        list_meetups_film #Film Meetups
       elsif input == "15"
-        list_meetups #Sci-Fi & Games Meetups
+        list_meetups_games #Sci-Fi & Games Meetups
       elsif input == "16"
-        list_meetups #Beliefs Meetups
+        list_meetups_beliefs #Beliefs Meetups
       elsif input == "17"
-        list_meetups #Arts Meetups
+        list_meetups_arts #Arts Meetups
       elsif input == "18"
-        list_meetups #Book Clubs Meetups
+        list_meetups_book_clubs #Book Clubs Meetups
       elsif input == "19"
-        list_meetups #Dance Meetups
+        list_meetups_dance #Dance Meetups
       elsif input == "20"
-        list_meetups #Pets Meetups
+        list_meetups_pets #Pets Meetups
       elsif input == "21"
-        list_meetups #Hobbies & Crafts Meetups
+        list_meetups_hobbies #Hobbies & Crafts Meetups
       elsif input == "22"
-        list_meetups #Fashion & Beauty Meetups
+        list_meetups_fashion #Fashion & Beauty Meetups
       elsif input == "23"
-        list_meetups #Social Meetups
+        list_meetups_social #Social Meetups
       elsif input == "24"
-        list_meetups #Career & Business Meetups
+        list_meetups_career #Career & Business Meetups
       elsif input == "list"
         list_categories
       elsif input == "exit"
@@ -86,6 +76,199 @@ class LocalAreaMeetups::CLI
         puts "Please enter a valid response!"
       end
     end
+  end
+
+  def list_meetups_outdoor
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_outdoor_meetups
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_tech
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_tech_meetups
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_family
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_family_meetups
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_health
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_health_and_wellness
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_learning
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_learning_meetups
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_sports
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_sports_and_fitness
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_photography
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_photograpy
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_food
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_food
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_writing
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_writing
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_language_culture
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_language_and_culture
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_music
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_music
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_movements
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_movements
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_lgbtq
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_lgbtq
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_film
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_film
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_games
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_games
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_beliefs
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_beliefs
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_arts
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_arts
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_book_clubs
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_book_clubs
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_dance
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_dance
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_pets
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_pets
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_hobbies
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_hobbies_and_crafts
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_fashion
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_fashion_and_beauty
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_social
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_social
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
+  end
+  def list_meetups_career
+    puts "Here is a list of meetups:"
+    puts "Loading..................."
+    @meetup = LocalAreaMeetups::Meetup.scrape_career_and_business
+    puts "Click the link for more information on the meetups listed below: #{@meetup.url}"
+    puts "#{@meetup.group_name}"
+    puts "Enjoy!!"
   end
 
   def goodbye
--- lib/local_area_meetups/meetup.rb
+++ lib/local_area_meetups/meetup.rb
@@ -1,354 +1,220 @@
 class LocalAreaMeetups::Meetup
-  attr_accessor :group_name, :url,
+  attr_accessor :group_name, :url
 
-  def self.meetups
-    self.scrape_meetups
-    self.scrape_outdoor_meetups
-
-    #self.scrape_tech_meetups
-    #self.scrape_family_meetups
-    #self.scrape_health_and_wellness
-    #self.scrape_learning_meetups
-    #self.scrape_sports_and_fitness
-    #self.scrape_photograpy
-    #self.scrape_food
-    #self.scrape_writing
-    #self.scrape_language_and_culture
-    ##self.scrape_music
-    ##self.scrape_movements
-    ##self.scrape_lgbtq
-    ##self.scrape_film
-    #self.scrape_games
-    #self.scrape_beliefs
-    #self.scrape_arts
-    #self.scrape_book_clubs
-    #self.scrape_dance
-    #self.scrape_pets
-    #self.scrape_hobbies_and_crafts
-    #self.scrape_fashion_and_beauty
-    #self.scrape_social
-    #self.scrape_career_and_business
-  end
-
-  def self.scrape_meetups
-    meetup = []
-
-    meetup << self.scrape_outdoor_meetups
-    meetup << self.scrape_tech_meetups
-    meetup << self.scrape_family_meetups
-    meetup << self.scrape_health_and_wellness
-    meetup << self.scrape_learning_meetups
-    meetup << self.scrape_sports_and_fitness
-    meetup << self.scrape_photograpy
-    meetup << self.scrape_food
-    meetup << self.scrape_writing
-    meetup << self.scrape_language_and_culture
-    meetup << self.scrape_music
-    meetup << self.scrape_movements
-    meetup << self.scrape_lgbtq
-    meetup << self.scrape_film
-    meetup << self.scrape_games
-    meetup << self.scrape_beliefs
-    meetup << self.scrape_arts
-    meetup << self.scrape_book_clubs
-    meetup << self.scrape_dance
-    meetup << self.scrape_pets
-    meetup << self.scrape_hobbies_and_crafts
-    meetup << self.scrape_fashion_and_beauty
-    meetup << self.scrape_social
-    meetup << self.scrape_career_and_business
-
-    meetup
-  end
 
   def self.scrape_outdoor_meetups
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/outdoors-adventure"))
     text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
-    binding.pry
-    i = 0
-    while i < 6
       outdoor_meetup = self.new
-      outdoor_meetup.group_name = "Name of each outdoor meetup should show here."#doc.search("ul.j-groupCard-list a").text
+      outdoor_meetup.group_name = "#{text}"#doc.search("ul.j-groupCard-list a").text
       outdoor_meetup.url = "https://www.meetup.com/find/outdoors-adventure"
       outdoor_meetup
-      i += 1
-    end
   end
 
   def self.scrape_tech_meetups
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/tech"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       tech_meetup = self.new
-      tech_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      tech_meetup.group_name = "#{text}"
       tech_meetup.url = "https://www.meetup.com/find/tech"
       tech_meetup
-      i += 1
-    end
   end
 
   def self.scrape_family_meetups
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/parents-family"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       family_meetup = self.new
-      family_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      family_meetup.group_name = "#{text}"
       family_meetup.url = "https://www.meetup.com/find/parents-family"
       family_meetup
-      i += 1
-    end
   end
 
   def self.scrape_health_and_wellness
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/health-wellness"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       health_meetup = self.new
-      health_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      health_meetup.group_name = "#{text}"
       health_meetup.url = "https://www.meetup.com/find/health-wellness"
       health_meetup
-      i += 1
-    end
   end
 
   def self.scrape_learning_meetups
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/education"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       learning_meetup = self.new
-      learning_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      learning_meetup.group_name = "#{text}"
       learning_meetup.url = "https://www.meetup.com/find/education"
-      @meetup
-      i += 1
-    end
+      learning_meetup
   end
 
   def self.scrape_sports_and_fitness
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/sports-fitness"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       sports_meetup = self.new
-      sports_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      sports_meetup.group_name = "#{text}"
       sports_meetup.url = "https://www.meetup.com/find/sports-fitness"
       sports_meetup
-      i += 1
-    end
   end
 
   def self.scrape_photograpy
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/photography"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       photography_meetup = self.new
-      photography_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      photography_meetup.group_name = "#{text}"
       photography_meetup.url = "https://www.meetup.com/find/photography"
       photography_meetup
-      i += 1
-    end
   end
 
   def self.scrape_food
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/food"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       food_meetup = self.new
-      food_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      food_meetup.group_name = "#{text}"
       food_meetup.url = "https://www.meetup.com/find/food"
       food_meetup
-      i += 1
-    end
   end
 
   def self.scrape_writing
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/writing"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       writing_meetup = self.new
-      writing_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      writing_meetup.group_name = "#{text}"
       writing_meetup.url = "https://www.meetup.com/find/writing"
       writing_meetup
-      i += 1
-    end
   end
 
   def self.scrape_language_and_culture
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/language"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       language_meetup = self.new
-      language_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      language_meetup.group_name = "#{text}"
       language_meetup.url = "https://www.meetup.com/find/language"
       language_meetup
-      i += 1
-    end
   end
 
   def self.scrape_music
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/music"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       music_meetup = self.new
-      music_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      music_meetup.group_name = "#{text}"
       music_meetup.url = "https://www.meetup.com/find/music"
       music_meetup
-      i += 1
-    end
   end
 
   def self.scrape_movements
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/movements"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       movements_meetup = self.new
-      movements_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      movements_meetup.group_name = "#{text}"
       movements_meetup.url = "https://www.meetup.com/find/movements"
       movements_meetup
-      i += 1
-    end
   end
 
   def self.scrape_lgbtq
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/lgbtq"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       lgbtq_meetup = self.new
-      lgbtq_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      lgbtq_meetup.group_name = "#{text}"
       lgbtq_meetup.url = "https://www.meetup.com/find/lgbtq"
       lgbtq_meetup
-      i += 1
-    end
   end
 
   def self.scrape_film
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/film"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       film_meetup = self.new
-      film_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      film_meetup.group_name = "#{text}"
       film_meetup.url = "https://www.meetup.com/find/film"
       film_meetup
-      i += 1
-    end
   end
 
   def self.scrape_games
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/games-sci-fi"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       games_meetup = self.new
-      games_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      games_meetup.group_name = "#{text}"
       games_meetup.url = "https://www.meetup.com/find/games-sci-fi"
       games_meetup
-      i += 1
-    end
   end
 
   def self.scrape_beliefs
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/beliefs"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       beliefs_meetup = self.new
-      beliefs_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      beliefs_meetup.group_name = "#{text}"
       beliefs_meetup.url = "https://www.meetup.com/find/beliefs/"
       beliefs_meetup
-      i += 1
-    end
   end
 
   def self.scrape_arts
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/arts-culture"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       arts_meetup = self.new
-      arts_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      arts_meetup.group_name = "#{text}"
       arts_meetup.url = "https://www.meetup.com/find/arts-culture"
       arts_meetup
-      i += 1
-    end
   end
 
   def self.scrape_book_clubs
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/book-clubs"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       book_clubs_meetup = self.new
-      book_clubs_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      book_clubs_meetup.group_name = "#{text}"
       book_clubs_meetup.url = "https://www.meetup.com/find/book-clubs"
       book_clubs_meetup
-      i += 1
-    end
   end
 
   def self.scrape_dance
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/dancing"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       dance_meetup = self.new
-      dance_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      dance_meetup.group_name = "#{text}"
       dance_meetup.url = "https://www.meetup.com/find/dancing"
       dance_meetup
-      i += 1
-    end
   end
 
   def self.scrape_pets
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/pets"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       pets_meetup = self.new
-      pets_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      pets_meetup.group_name = "#{text}"
       pets_meetup.url = "https://www.meetup.com/find/pets"
       pets_meetup
-      i += 1
-    end
   end
 
   def self.scrape_hobbies_and_crafts
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/hobbies-crafts"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       hobbies_meetup = self.new
-      hobbies_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      hobbies_meetup.group_name = "#{text}"
       hobbies_meetup.url = "https://www.meetup.com/find/hobbies-crafts"
       hobbies_meetup
-      i += 1
-    end
   end
 
   def self.scrape_fashion_and_beauty
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/fashion-beauty"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       fashion_meetup = self.new
-      fashion_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      fashion_meetup.group_name = "#{text}"
       fashion_meetup.url = "https://www.meetup.com/find/fashion-beauty"
       fashion_meetup
-      i += 1
-    end
   end
 
   def self.scrape_social
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/social"))
-    i = 0
-    while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       social_meetup = self.new
-      social_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      social_meetup.group_name = "#{text}"
       social_meetup.url = "https://www.meetup.com/find/social"
       social_meetup
-      i += 1
-    end
   end
 
   def self.scrape_career_and_business
     doc = Nokogiri::HTML(open("https://www.meetup.com/find/career-business"))
-    #i = 0
-    #while i < 6
+    text = doc.search("div.unit h3").text.gsub("\n", ' ').gsub("\t", ' ').split.join(' ')
       career_meetup = self.new
-      career_meetup.group_name = doc.search("ul.j-groupCard-list a").text
+      career_meetup.group_name = "#{text}"
       career_meetup.url = "https://www.meetup.com/find/career-business/"
-      #@meetup_meetups << career_meetup
-      #i += 1
       career_meetup
-    #end
   end
 end
