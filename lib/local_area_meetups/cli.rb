20d2909ce1b5f2a7aef5a687353935c1f07a7f9d
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-11-14 18:43:58 +0000

    refactoring CLI file

--- lib/local_area_meetups/cli.rb
+++ lib/local_area_meetups/cli.rb
@@ -3,97 +3,71 @@ class LocalAreaMeetups::CLI
   def call
     welcome
     #scrape first level
-    list_categories
-    menu
+    choose_category
+    category_menu
   end
 
   def welcome
-    puts "-----------------------------------------"
-    puts ""
-    puts "Welcome to my First Ruby CLI Meetup Gem"
-    puts ""
-    puts "-----------------------------------------"
+    puts " ================================================"
+    puts "|                                                |"
+    puts "|    Welcome to my First Ruby CLI Meetup Gem     |"
+    puts "|                                                |"
+    puts " ==============================================="
+  end
+
+  def choose_category
+    #asks the user to choose a category.
+    puts "Which Category are you interested in?"
+    list_categories
   end
 
   def list_categories
-    puts "Choose from the category list (1 - 24)"
-    puts ""
+    puts "Choose from the category list (1-24)"
+    puts " "
     LocalAreaMeetups::Scraper.scrape_categories.each.with_index(1) do |category, i|
       puts "#{i}. #{category}"
     end
   end
 
-  def menu
+  def self.list_meetups
+    puts "Choose a Meetup"
+    puts ""
+    LocalAreaMeetups::Scraper.scrape_groups.each.with_index(1) do |meetup, i|
+      puts "#{i}. #{meetup}"
+    end
+    binding.pry
+  end
+
+  def category_menu
     input = nil
     while input != "exit"
       puts "Choose list to view the categories again or type exit to exit:"
       input = gets.strip.downcase
       puts " "
-      #case input
-      #when "list"
-        #list_categories
-      #when "exit"
-      #  goodbye
-      #  break
-    #  else
-      #  system("open #{@meetups.add_meetups[input.toi-1]}")
-      if input == "1"
-        #check if I have scraped Category 1's meetups
-        LocalAreaMeetups::Scraper.scrape_meetups("outdoors-adventure").each.with_index(1) do |meetup, i|
-          puts "#{i}. #{meetup.group_name} - #{meetup.url} - #{meetup.members}"
-        end
-      elsif input == "2"
-        LocalAreaMeetups::Scraper.scrape_meetups("tech")
-      elsif input == "3"
-        LocalAreaMeetups::Scraper.scrape_meetups("parents-family")
-      elsif input == "4"
-        LocalAreaMeetups::Scraper.scrape_meetups("health-wellness")
-      elsif input == "5"
-        LocalAreaMeetups::Scraper.scrape_meetups("education")
-      elsif input == "6"
-        LocalAreaMeetups::Scraper.scrape_meetups("sports-fitness")
-      elsif input == "7"
-        LocalAreaMeetups::Scraper.scrape_meetups("photography")
-      elsif input == "8"
-        list_meetups_food #Food & Drink Meetups
-      elsif input == "9"
-        list_meetups_writing #Writing Meetups
-      elsif input == "10"
-        list_meetups_language_culture #Language & Culture Meetups
-      elsif input == "11"
-        list_meetups_music #Music Meetups
-      elsif input == "12"
-        list_meetups_movements #Movements Meetups
-      elsif input == "13"
-        list_meetups_lgbtq #LGBTQ Meetups
-      elsif input == "14"
-        list_meetups_film #Film Meetups
-      elsif input == "15"
-        list_meetups_games #Sci-Fi & Games Meetups
-      elsif input == "16"
-        list_meetups_beliefs #Beliefs Meetups
-      elsif input == "17"
-        list_meetups_arts #Arts Meetups
-      elsif input == "18"
-        list_meetups_book_clubs #Book Clubs Meetups
-      elsif input == "19"
-        list_meetups_dance #Dance Meetups
-      elsif input == "20"
-        list_meetups_pets #Pets Meetups
-      elsif input == "21"
-        list_meetups_hobbies #Hobbies & Crafts Meetups
-      elsif input == "22"
-        list_meetups_fashion #Fashion & Beauty Meetups
-      elsif input == "23"
-        list_meetups_social #Social Meetups
-      elsif input == "24"
-        list_meetups_career #Career & Business Meetups
-      elsif input == "list"
+      if input == "list"
         list_categories
       elsif input == "exit"
         goodbye
       else
         puts "Please enter a valid response!"
+      end
+    end
+  end
+
+  def meetup_menue
+    input = nil
+    while input != "categories"
+      puts "Choose a meetup"
+      input = gets.strip.downcase
+      puts ""
+      if input == "1"
+        list_meetups
+      elsif input == "categories"
+        list_categories
+      elsif input == "meetups"
+        list_meetups
+      elsif input == "exit"
+        goodbye
       end
     end
   end
--- lib/local_area_meetups/scraper.rb
+++ lib/local_area_meetups/scraper.rb
@@ -8,55 +8,20 @@ class LocalAreaMeetups::Scraper
     names = doc.search(".gridList-item").map(&:text)[0..-13]
   end
   ####################Meetup Scraper#######################
-  def self.get_meetup_page
-    doc = Nokogiri::HTML(open("https://www.meetup.com"))
-    n = doc.search(".gridList-item")[0..-13]
-    div = n.css(".exploreHome-categories-card")
-    urls = div.css(".exploreHome-categories-card a").map { |anchor| anchor["href"] }
-
-    meetup_website = urls.each do |cat|
-
-    info = urls.css("h3 .padding-none inline-block loading").text.strip
-    binding.pry
-  end
-
-  def self.get_meetup_links
-    l = self.get_meetup_page
-    link = l.css(".exploreHome-categories-card")
-    #to collect all of the href values at once.
-    hrefs = link.css(".exploreHome-categories-card a").map { |anchor| anchor["href"] }
-  end
-
-  def self.make_meetups
-      self.get_meetup_links.each do |links|
-        meetup = LocalAreaMeetups::Meetup.new
-        meetup.group_name = links.search("h3 .padding-none inline-block loading").text.strip
-        meetup.members = links.search('p').map(&:text)
-        #binding.pry
-        meetup.url = "#{links}"
-        #binding.pry
+  def self.scrape_meetups(category)
+    category =  "Tech"
+		doc = Nokogiri::HTML(open("https://www.meetup.com/find/#{category}"))
+    group = doc.search("h3").map(&:text).each.with_index(1) do |meetup, i|
+      puts "#{i}. #{meetup}"
     end
-  end
+	end
 
-  def self.print_meetups
-    self.make_meetups
-    LocalAreaMeetups::Meetup.all.each do |meetup|
-      if meetup.group_name
-        puts "Group Name: #{meetup.group_name}"
-        puts "  Group Members: #{meetup.members}"
-        puts "  Group URL: #{meetup.url}"
-        #binding.pry
+    def self.scrape_groups(groups)
+      groups.each_with_index(1) do |group_info, i|
+        puts "#{i}. #{group_info.group_name}"
+        puts "          #{group_info.member}"
+        puts "          #{group_info.url}"
       end
-    end
-  end
-
-  #def self.scrape_meetups(category)
-		#doc = Nokogiri::HTML(open("https://www.meetup.com/find/#{category}"))
-		#doc.css(".groupCard tileGrid-tile noRatings").each do |meetup|
-    #m = LocalAreaMeetups::Meetup.new
-		#m.group_name = meetup.css("h3 .padding-none inline-block loading").text.strip
-		#m.members = doc.search('p').map(&:text)
-		#m.url = "https://www.meetup.com/find/#{category}"
-		#end
-	#end
+      binding.pry
+	  end
 end
