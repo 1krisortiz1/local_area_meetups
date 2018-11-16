3c6a819c8e1ce15ed81104115a71d1bc63f641cb
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-11-16 16:01:01 +0000

    updating scraper method to scrape group info

--- lib/local_area_meetups/meetup.rb
+++ lib/local_area_meetups/meetup.rb
@@ -1,25 +1,22 @@
 class LocalAreaMeetups::Meetup
 
   attr_accessor :group_name, :url, :category, :member
+
   @@all = []
+
+  def initialize(group_name, url, category, members)
+    @group_name = Meetup.group_name
+    @url = Meetup.url
+  #  @category = LocalAreaMeetups::Category.find_by_name(category)
+    @members = Meetup.members
+    @@all << self
+  end
 
   def self.all
     @@all
   end
 
-  def save #instance method
-    @@all << self
-  end
-
   def find_by_name(name)
     self.all.detect{|o| o.name == name}
-  end
-
-  def initialize(group_name, url, category, members)
-    @group_name = Meetup.group_name
-    @url = Meetup.url
-    @category = LocalAreaMeetups::Category.find_by_name(category)
-    @members = Meetup.members
-    save
   end
 end
--- lib/local_area_meetups/scraper.rb
+++ lib/local_area_meetups/scraper.rb
@@ -1,6 +1,4 @@
 class LocalAreaMeetups::Scraper
-
-
 ####################Category Scraper#######################
   def self.scrape_categories
     puts "scraping"
@@ -8,29 +6,30 @@ class LocalAreaMeetups::Scraper
     names = doc.search(".gridList-item").map(&:text)[0..-13]
   end
   ####################Meetup Scraper#######################
-  def self.scrape_meetups(category)
+  def self.scrape_meetup(category)
+    category = "Tech"
     @category = category
     @meetups = []
 		doc = Nokogiri::HTML(open("https://www.meetup.com/find/#{category}"))
-    groups = doc.search("div a.display-none").map(&:text)[1..20]
+    groups_name = doc.search("div a.display-none").map(&:text)[1..20]
+binding.pry
+
     #@meetups << groups
     #puts @meetups
 	end
 
-    def self.scrape_groups#(groups)
+    def self.scrape_groups(input)
+      #based on user input will select that group number and print
       puts "Here is the information you requested:"
       puts ""
-      puts "#{Meetup.name}"
+      puts "#{group_name.name}"
       puts "#{Meetup.date_time}"
       puts "#{Meetup.details}"
       puts ""
     end
-
+end
       #groups.each_with_index(1) do |group_info, i|
       #  puts "#{i}. #{group_info.group_name}"
       #  puts "          #{group_info.member}"
       #  puts "          #{group_info.url}"
       #end
-      #binding.pry
-	  #end
-end
